d86d386291071b7dec01f67ba935aeb7dd186690
balankdharan <balankdharan@gmail.com>
2024-06-06 20:46:28 +0530

    Ui updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import TransactionPage from "./pages/TransactionPage";
+import Header from "./components/ui/Header";
 function App() {
-  return <h1 className="text-red-500 text-5xl">Helo thre</h1>;
+  const authUser = true;
+  return (
+    <>
+      {authUser && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
